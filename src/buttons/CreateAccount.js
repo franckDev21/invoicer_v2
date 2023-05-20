3d3075d5eb6dcc07ae6b52175dc33324263fcd20
franckDev21 <82069062+franckDev21@users.noreply.github.com>
2023-05-20 09:42:09 +0100

    Update CreateAccount.js

--- src/buttons/CreateAccount.js
+++ src/buttons/CreateAccount.js
@@ -10,8 +10,9 @@ export default function CreateAccount() {
         onClick={login}
         className="bg-blue-500 py-2 px-6 rounded text-base hover:bg-transparent text-white transition-all duration-150 hover:bg-blue-600 shadow-lg hover:ring-4 hover:ring-blue-400"
       >
-        Log In or Create Account
+        Log In or Create Account 
       </button>
     </>
   );
 }
+
